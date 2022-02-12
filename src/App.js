8ea64b1af0fa2aafbf8a36b9804d06b1435ca667
Jos02378 <haryantojoseph@gmail.com>
2022-02-12 22:39:15 +0700

    Feat: Add owl carousel to credit page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import Result from './pages/Result/Result';
 import Contact from './pages/Contact/Contact';
 import Sidebar from './components/Sidebar';
 import Credits from './pages/Credits/Credits';
+import TestiMonials from './components/TestiMonials/TestiMonials';
+import reactDom from 'react-dom';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -31,6 +33,7 @@ function App() {
         <Route path={ROUTES.IMAGE_INDIVIDUAL} element={<DropFileInput />} />
         <Route path={ROUTES.IMAGE_FOLDER} element={<FolderDetection />} />
         <Route path={ROUTES.TEXT_DETECTION} element={<TextDetection />} />
+        <Route path='/testimonials' element={<TestiMonials />} />
         <Route path={ROUTES.RESULT} element={<Result />} />
         <Route path={ROUTES.CONTACT} element={<Contact />} />
         <Route path={ROUTES.CREDITS} element={<Credits />} />
