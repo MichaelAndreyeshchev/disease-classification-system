9ff138904f8542ed07f3169876447a6771e42c7e
Jos02378 <haryantojoseph@gmail.com>
2022-02-22 22:40:00 +0700

    Feat: Create profile card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from './Card.module.css';
 import avatar from '../../Images/avatar.png';
+import avatar2 from '../../Images/michael_avatar.png';
 
-const Card = () => {
+const Card = ({ name, role, description, variant }) => {
   return (
     <div className={styles.card}>
-      <img className={styles['card__image']} src={avatar} alt='' />
+      <img
+        className={styles['card__image']}
+        src={variant == 'michael' ? avatar2 : avatar}
+        alt=''
+      />
       <div className={styles['card__container']}>
-        <span className={styles['card__title']}>Joseph</span>
-        <span className={styles['card__subtitle']}>Role</span>
-        <span className={styles['card__description']}>Description</span>
+        <span className={styles['card__title']}>{name}</span>
+        <span className={styles['card__subtitle']}>{role}</span>
+        <span className={styles['card__description']}>{description}</span>
       </div>
     </div>
   );
