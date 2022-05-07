f429524446d1161e1cc78e46ab8f0575a3677473
Jos02378 <haryantojoseph@gmail.com>
2022-05-07 20:23:35 +0700

    Feat: add multiple download formats

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,16 +1,108 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FiX } from 'react-icons/fi';
 import styles from './Modal.module.css';
 import ModalButton from '../Button/ModalButton';
 
+import { CSVLink } from 'react-csv';
+import * as XLSX from 'xlsx';
+
+/*
+- csv (Done)
+- txt (DONE)
+- xlsx (Done)
+
+ */
+
 const Modal = (props) => {
+  const data = [
+    { firstname: 'Ahmed', lastname: 'Tomi', email: 'ah@smthing.co.com' },
+    { firstname: 'Raed', lastname: 'Labes', email: 'rl@smthing.co.com' },
+    { firstname: 'Yezzi', lastname: 'Min l3b', email: 'ymin@cocococo.com' },
+  ];
+
+  const headers = [
+    { label: 'First Name', key: 'firstname' },
+    { label: 'Last Name', key: 'lastname' },
+    { label: 'Email', key: 'email' },
+  ];
+
+  const csvReport = {
+    filename: 'MyReport.csv',
+    headers: headers,
+    data: data,
+  };
+
+  const [wikiData, setWikiData] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [fileFormat, setFileFormat] = useState('csv');
+  const csvLink = useRef(null);
+
+  const LearnMoreUrl = `https://www.verywellhealth.com/organ-system-1298691#toc-${props.data.tag.toLowerCase()}-system`;
+
   useEffect(() => {
+    setIsLoading(true);
     console.log(props.parent);
     console.log(props.cardPosition);
     if (props.cardPosition) {
       props.parent.current.to(props.cardPosition, 0);
     }
+
+    getData(props.data.name.toLowerCase());
   }, []);
+
+  const handleDownload = () => {
+    if (fileFormat === 'csv') {
+      csvLink.current.link.click();
+    } else if (fileFormat === 'txt') {
+      downloadTxtFile();
+    } else if (fileFormat === 'xlsx') {
+      downloadExcelFile();
+    } else if (fileFormat === 'json') {
+      downloadJsonFile();
+    }
+  };
+
+  const downloadTxtFile = () => {
+    const element = document.createElement('a');
+    const file = new Blob(['test'], { type: 'text/plain;charset-utf-8' });
+
+    element.href = URL.createObjectURL(file);
+    element.download = 'MyReport.txt';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
+  const downloadExcelFile = () => {
+    var wb = XLSX.utils.book_new();
+    var ws = XLSX.utils.json_to_sheet(data);
+
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
+
+    XLSX.writeFile(wb, 'MyReport.xlsx');
+  };
+
+  const downloadJsonFile = () => {
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(data, null, 2)
+    )}`;
+    const link = document.createElement('a');
+    link.href = jsonString;
+    link.download = 'MyReport.json';
+    link.click();
+  };
+
+  const getData = async (title) => {
+    const response = await fetch(
+      `https://en.wikipedia.org/w/api.php?&origin=*&action=query&prop=extracts&format=json&exintro=&titles=${title}`
+    ).then((response) => response.json());
+
+    setWikiData(Object.values(response.query.pages)[0].extract);
+    setIsLoading(false);
+    console.log(response);
+    console.log(response.query.pages);
+    console.log(Object.values(response.query.pages)[0].extract);
+  };
 
   return (
     <>
@@ -32,7 +124,129 @@ const Modal = (props) => {
             </div>
           </div>
           <div className={styles.modal__body}>
-            <div className={styles.wrapper}>
+            {props.variant === 'download' ? (
+              <div
+                className={styles.wrapper + ' ' + styles['wrapper--download']}
+              >
+                <h2>file format</h2>
+                <div
+                  className={styles.wrapper + ' ' + styles['wrapper--buttons']}
+                >
+                  <label
+                    class={
+                      styles['input-button'] +
+                      ' ' +
+                      (fileFormat === 'csv' && styles['active'])
+                    }
+                  >
+                    <input
+                      type='radio'
+                      name='options'
+                      id='option1'
+                      className={styles['radio-button']}
+                      autocomplete='off'
+                      value='csv'
+                      onClick={(e) => setFileFormat(e.target.value)}
+                    />
+                    csv
+                  </label>
+                  <label
+                    class={
+                      styles['input-button'] +
+                      ' ' +
+                      (fileFormat === 'txt' && styles['active'])
+                    }
+                  >
+                    <input
+                      type='radio'
+                      name='options'
+                      id='option1'
+                      className={styles['radio-button']}
+                      autocomplete='off'
+                      value='txt'
+                      onClick={(e) => setFileFormat(e.target.value)}
+                    />
+                    txt
+                  </label>
+                  <label
+                    class={
+                      styles['input-button'] +
+                      ' ' +
+                      (fileFormat === 'xlsx' && styles['active'])
+                    }
+                  >
+                    <input
+                      type='radio'
+                      name='options'
+                      id='option2'
+                      className={styles['radio-button']}
+                      autocomplete='off'
+                      value='xlsx'
+                      onClick={(e) => setFileFormat(e.target.value)}
+                    />
+                    xlsx
+                  </label>
+                  <label
+                    class={
+                      styles['input-button'] +
+                      ' ' +
+                      (fileFormat === 'json' && styles['active'])
+                    }
+                  >
+                    <input
+                      type='radio'
+                      name='options'
+                      id='option3'
+                      className={styles['radio-button']}
+                      autocomplete='off'
+                      value='json'
+                      onClick={(e) => setFileFormat(e.target.value)}
+                    />
+                    json
+                  </label>
+                </div>
+                <CSVLink
+                  {...csvReport}
+                  ref={csvLink}
+                  className={styles['button--csv']}
+                >
+                  Download me
+                </CSVLink>
+                <ModalButton
+                  text='Download'
+                  onClick={handleDownload}
+                  variant='reject'
+                />
+              </div>
+            ) : (
+              <div>
+                {isLoading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <article>
+                    <div>
+                      <h2>Disease: {props.data.name}</h2>
+                      <h3>Body system: {props.data.tag} system</h3>
+                      <h4>Probability: {props.data.percentage}%</h4>
+                    </div>
+                    <div
+                      className={styles['body__wrapper']}
+                      dangerouslySetInnerHTML={{ __html: wikiData && wikiData }}
+                    ></div>
+                    <br />
+                    <div>
+                      <h3>
+                        Don't know some of the words here?{' '}
+                        <a href={LearnMoreUrl} target='_blank'>
+                          Learn more
+                        </a>
+                      </h3>
+                    </div>
+                  </article>
+                )}
+              </div>
+            )}
+            {/* <div className={styles.wrapper}>
               <h1>This is modal body</h1>
               <ul className={styles.list}>
                 <li>
@@ -41,7 +255,7 @@ const Modal = (props) => {
                   </a>
                 </li>
               </ul>
-            </div>
+            </div> */}
           </div>
           <div className={styles.modal__footer}>
             <ModalButton
