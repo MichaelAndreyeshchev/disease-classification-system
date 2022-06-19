976ba692605eb23f00fc3c8b84cf0751ff7008a4
Joseph Haryanto <haryantojoseph@gmail.com>
2022-06-19 22:06:29 +0700

    Feat: add card for home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,9 @@ import Button from '../../components/Button/Button';
 import LinkCustom from '../../components/Link/Link';
 import * as ROUTES from '../../Constants/Routes';
 import styles from './Home.module.css';
-import home_image from '../../Images/home_image.png';
+import home_image from '../../Images/home_image2.png';
+import text_option from '../../Images/text_option.png';
+import image_option from '../../Images/image_option.png';
 
 function Home() {
   return (
@@ -12,19 +14,18 @@ function Home() {
         <artivle className={styles.wrapper}>
           <div>
             <h1 className={styles.wrapper__title}>
-              Don’t worry, We always got you covered!
+              Don’t worry, We always got you{' '}
+              <span className={styles['text-highlight']}>covered</span>!
             </h1>
           </div>
           <div>
-            <p>
+            <p className={styles.wrapper__description}>
               Welcome to{' '}
-              <span className={styles['description--highlight']}>
-                Disease Tracker
-              </span>
-              , this is a sophisticated and intuitive tool that you can always
+              <span className={styles['text-highlight']}>Disease Tracker</span>,
+              this is a sophisticated and intuitive tool that you can always
               rely on whenever you’re in a pinch!
             </p>
-            <p>
+            <p className={styles.wrapper__description}>
               To start off, please write down the symptoms that you have been
               experienced below.
             </p>
@@ -36,27 +37,25 @@ function Home() {
       </section>
 
       <section className={styles.container + ' ' + styles.section__features}>
-        <div
-          className={styles.container__wrapper + ' ' + styles['wrapper--left']}
-        >
-          <LinkCustom
-            to={ROUTES.IMAGE_INDIVIDUAL}
-            text='Image Based'
-            iconPosition='left'
-            size='big'
-          />
-        </div>
-
-        <div
-          className={styles.container__wrapper + ' ' + styles['wrapper--right']}
-        >
-          <LinkCustom
-            to={ROUTES.TEXT_DETECTION}
-            text='Text Based'
-            iconPosition='right'
-            size='big'
-          />
-        </div>
+        <h2>Tell us your preferred way to get your disease diagnosed</h2>
+        <article className={styles.wrapper + ' ' + styles['wrapper--row']}>
+          <div className={styles.option}>
+            <img className={styles.option__image} src={text_option} alt='' />
+            <h3 className={styles.option__title}>Write down symptoms</h3>
+            <p className={styles.option__description}>
+              Tell us every symptom that you are currently facing in the
+              provided textbox.
+            </p>
+          </div>
+          <div className={styles.option + ' ' + styles['option--right']}>
+            <img className={styles.option__image} src={image_option} alt='' />
+            <h3 className={styles.option__title}>Upload images of disease</h3>
+            <p className={styles.option__description}>
+              Upload every picture that is related to the disease you are
+              currently hosting.
+            </p>
+          </div>
+        </article>
       </section>
     </main>
   );
