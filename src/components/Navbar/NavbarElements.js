2156f0ee36a95e4cbb9354d691bf20781c54caf9
Joseph Haryanto <haryantojoseph@gmail.com>
2022-07-15 16:29:46 +0700

    feat: add styling for navbar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,26 +3,39 @@ import { NavLink as Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 
 export const Nav = styled.nav`
-  background: #fff;
-  height: 80px;
   display: flex;
   justify-content: space-between;
-  padding: 0.5rem calc((100vw - 1200px) / 2);
+  width: 100%;
+  height: 80px;
+  position: absolute;
+  top: 0;
+  padding: 0.5rem calc((100vw - 1500px) / 2);
+  border: 1px solid red;
   z-index: 10;
 `;
 
 export const NavLink = styled(Link)`
-  color: #000;
   display: flex;
   align-items: center;
   text-decoration: none;
-  padding: 0 1rem;
   height: 100%;
   cursor: pointer;
+  padding: 0 1em;
   transition: transform 0.25s ease-in-out;
 
+  font-family: 'Nunito';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.2rem;
+  line-height: 41px;
+  text-align: center;
+  color: #09ac91;
+
   &.active {
-    font-weight: 700;
+    color: #3ee7ba;
+    text-decoration: underline;
+    text-decoration-thickness: 2px;
+    text-underline-offset: 5px;
   }
 
   &:hover {
@@ -34,7 +47,7 @@ export const Bars = styled(FaBars)`
   display: none;
   color: #c4c4c4;
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1024px) {
     display: block;
     position: absolute;
     top: 0;
@@ -48,9 +61,10 @@ export const Bars = styled(FaBars)`
 export const NavMenu = styled.div`
   display: flex;
   align-items: center;
-  margin-right: -24px;
+  justify-content: space-around;
+  width: clamp(37rem, 60vw, 50rem);
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1024px) {
     display: none;
   }
 `;
