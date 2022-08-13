5e0e9f706aea374157397580d94d1eba7c5f1340
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2022-08-14 00:03:13 +0700

    feat: change styling and add background

--- src/pages/Result/Result.js
+++ src/pages/Result/Result.js
@@ -5,6 +5,7 @@ import * as ROUTES from '../../Constants/Routes';
 import styles from './Result.module.css';
 import { BsFillArrowDownCircleFill, BsDownload } from 'react-icons/bs';
 import Modal from '../../components/Modal/Modal';
+import background from '../../Images/Vector 10.png';
 
 function Result() {
   const [modalClicked, setModalClicked] = useState(false);
@@ -135,34 +136,68 @@ function Result() {
 
   return (
     <main className={styles.main}>
-      <article className={styles.container}>
-        <h2 className={styles['container__heading--result']}>You might have</h2>
-        <h1 className={styles.container__result}>
-          Tubercolosis
-          <spans className={styles.container__percentages}>70%</spans>
-        </h1>
+      <article className={styles.container + ' ' + styles['container--left']}>
+        <h2 className={styles['container__heading--result']}>you might have</h2>
+        <h1 className={styles.container__result}>Tubercolosis!</h1>
+        <h3
+          className={
+            styles.container__subheading + ' ' + styles['subheading--small']
+          }
+        >
+          <span className={styles.container__percentages}>92%</span> matched
+          your symptoms. <span className={styles.link}>Read more</span>
+        </h3>
       </article>
 
-      <article className={styles.container}>
-        <h2 className={styles.container__heading}>Description</h2>
-        <p className={styles.container__description}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          accumsan at tortor at gravida. Nulla at metus massa. Duis vestibulum
-          interdum sagittis. Ut at feugiat lorem. Class aptent taciti sociosqu
-          ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
-          rutrum nec lacus sed malesuada. Nunc imperdiet accumsan ipsum quis
-          egestas. Maecenas malesuada luctus libero, eget porta lorem tempus id.
-          Praesent vulputate odio quis mauris euismod volutpat. Nunc feugiat
-          justo eu magna dictum tincidunt. Nam sollicitudin turpis a nulla
-          tempor viverra. Integer vehicula ante vulputate, sollicitudin risus
-          et, consequat enim. Praesent a ipsum libero. Integer iaculis nibh
-          cursus, rhoncus tortor eu, tempus metus. Nam libero justo, commodo
-          porta placerat vitae, cursus eget libero. Maecenas ultricies mattis
-          justo in euismod. Vivamus eget libero tempus sem scelerisque
-          pellentesque ac vitae mauris. Duis sed est ipsum. Cras vulputate
-          porttitor ante et varius. Nunc at nunc ac metus commodo elementum.
-          Curabitur fermentum ut lectus.
-        </p>
+      <article
+        className={styles.container + ' ' + styles['container--background']}
+      >
+        <img className={styles.container__background} src={background} alt='' />
+        <div className={styles.wrapper}>
+          <h2 className={styles.container__heading}>What is Tuberculosis?</h2>
+          <h3
+            className={
+              styles.container__subheading + ' ' + styles['subheading--yellow']
+            }
+          >
+            Overview
+          </h3>
+          <p className={styles.container__description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            accumsan at tortor at gravida. Nulla at metus massa. Duis vestibulum
+            interdum sagittis. Ut at feugiat lorem. Class aptent taciti sociosqu
+            ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
+            rutrum nec lacus sed malesuada. Nunc imperdiet accumsan ipsum quis
+            egestas. Maecenas malesuada luctus libero, eget porta lorem tempus
+            id. Praesent vulputate odio quis mauris euismod volutpat. Nunc
+            feugiat justo eu magna dictum tincidunt. Nam sollicitudin turpis a
+            nulla tempor viverra. Integer vehicula ante vulputate, sollicitudin
+            risus et, consequat enim. Praesent a ipsum libero. Integer iaculis
+            nibh cursus, rhoncus tortor eu, tempus metus. Nam libero justo,
+            commodo porta placerat vitae, cursus eget libero. Maecenas ultricies
+            mattis justo in euismod. Vivamus eget libero tempus sem scelerisque
+            pellentesque ac vitae mauris. Duis sed est ipsum. Cras vulputate
+            porttitor ante et varius. Nunc at nunc ac metus commodo elementum.
+            Curabitur fermentum ut lectus.
+          </p>
+          <h3
+            className={
+              styles.container__subheading + ' ' + styles['subheading--yellow']
+            }
+          >
+            Your next steps
+          </h3>
+          <p className={styles.container__description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            accumsan at tortor at gravida. Nulla at metus massa. Duis vestibulum
+            interdum sagittis. Ut at feugiat lorem. Class aptent taciti sociosqu
+            ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
+            rutrum nec lacus sed malesuada. Nunc imperdiet accumsan ipsum quis
+            egestas. Maecenas malesuada luctus libero, eget porta lorem tempus
+            id. Praesent vulputate odio quis mauris euismod volutpat. Nunc
+            feugiat justo eu magna dictum tincidunt.
+          </p>
+        </div>
       </article>
 
       <article className={styles.container + ' ' + styles['container--result']}>
