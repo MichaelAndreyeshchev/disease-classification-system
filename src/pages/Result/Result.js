31c929c6870b735ade468381c356f347543f4d1b
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2022-08-26 22:41:18 +0700

    refactor: change article element to section

--- src/pages/Result/Result.js
+++ src/pages/Result/Result.js
@@ -140,7 +140,7 @@ function Result() {
 
   return (
     <main className={styles.main}>
-      <article className={styles.container + ' ' + styles['container--left']}>
+      <section className={styles.container + ' ' + styles['container--left']}>
         <h2 className={styles['container__heading--result']}>you might have</h2>
         <h1 className={styles.container__result}>Tubercolosis!</h1>
         <h3
@@ -151,9 +151,9 @@ function Result() {
           <span className={styles.container__percentages}>92%</span> matched
           your symptoms. <span className={styles.link}>Read more</span>
         </h3>
-      </article>
+      </section>
 
-      <article
+      <section
         className={styles.container + ' ' + styles['container--background']}
       >
         <img className={styles.container__image} src={healthCheck} alt='' />
@@ -192,57 +192,82 @@ function Result() {
           >
             Your next steps
           </h3>
-          <p className={styles.container__description}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            accumsan at tortor at gravida. Nulla at metus massa. Duis vestibulum
-            interdum sagittis. Ut at feugiat lorem. Class aptent taciti sociosqu
-            ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
-            rutrum nec lacus sed malesuada. Nunc imperdiet accumsan ipsum quis
-            egestas. Maecenas malesuada luctus libero, eget porta lorem tempus
-            id. Praesent vulputate odio quis mauris euismod volutpat. Nunc
-            feugiat justo eu magna dictum tincidunt.
+          <p
+            className={
+              styles.container__description +
+              ' ' +
+              styles['description--big-margin']
+            }
+          >
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim,
+            dolorum sequi nobis optio perferendis ex veritatis placeat velit
+            soluta eos aliquam officia, quam error in assumenda a ipsa delectus
+            cupiditate, maiores accusamus sunt voluptates provident nemo?
+            Deleniti sequi sunt optio corporis labore ipsam iste vero voluptas,
+            cumque quasi repellat enim neque rerum eum odit mollitia illum
+            quibusdam sit aliquam esse natus excepturi! Veritatis eum, optio
+            necessitatibus nihil quia sequi cumque cupiditate delectus tempora
+            voluptatibus, voluptatem veniam? Eveniet officia suscipit
+            perferendis assumenda nostrum id, cupiditate quidem a molestiae!
+            Quibusdam totam voluptatibus assumenda dolore fugit dolorum. Et
+            delectus itaque accusamus perspiciatis accusantium eaque omnis
+            mollitia asperiores saepe eius minus tenetur sequi iste sapiente hic
+            quidem nulla fugit distinctio, quaerat optio! Voluptas, ipsa rem
+            illo obcaecati ullam accusamus quas corporis enim nemo? Sequi earum
+            aut quis et, voluptate voluptatibus tempore dolorem iste, quisquam
+            repellat dolorum aliquid itaque eos quas doloribus inventore!
+            Quaerat quasi quas, consectetur fugit dicta doloribus molestiae
+            porro earum mollitia asperiores? Dolorem esse assumenda nulla,
+            officiis unde velit dolore, modi ea mollitia magni voluptatem itaque
+            similique aspernatur saepe labore. Dolor accusantium temporibus
+            nesciunt natus debitis tempora maiores nobis vel, quas omnis quis
+            blanditiis, doloribus alias earum qui voluptatem nihil officia
+            facilis?
           </p>
         </div>
-      </article>
+      </section>
 
-      <article className={styles.container + ' ' + styles['container--result']}>
-        <article className={styles['container__header']}>
-          <div>
-            <h2 className={styles.container__heading}>Other Top Results</h2>
-            <p
-              className={
-                styles.container__description + ' ' + styles['no--margin']
-              }
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              enim amet, maecenas ac. Ipsum mauris tortor nulla ultrices quisque
-              dictum. At odio consequat tellus mauris ullamcorper sed. Nibh non
-              interdum cursus ornare pretium ut.
-            </p>
-          </div>
+      <section className={styles.container}>
+        <article
+          className={styles.container + ' ' + styles['container--result']}
+        >
+          <article className={styles['container__header']}>
+            <div>
+              <h2 className={styles.container__heading}>Other Top Results</h2>
+              <p
+                className={
+                  styles.container__description + ' ' + styles['no--margin']
+                }
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                enim amet, maecenas ac. Ipsum mauris tortor nulla ultrices
+                quisque dictum. At odio consequat tellus mauris ullamcorper sed.
+                Nibh non interdum cursus ornare pretium ut.
+              </p>
+            </div>
 
-          <div>
-            <label for='numOfData'>Number Of Data:</label>
+            <div>
+              <label for='numOfData'>Number Of Data:</label>
 
-            <select
-              name='numOfData'
-              id='numOfData'
-              onChange={(e) => {
-                setNumberOfAddition(e.target.value);
-              }}
-            >
-              <option value='3'>3</option>
-              <option value='6'>6</option>
-              <option value='9'>9</option>
-              <option value='12'>12</option>
-              <option value='15'>15</option>
-              <option value='15'>All</option>
-            </select>
-          </div>
-        </article>
+              <select
+                name='numOfData'
+                id='numOfData'
+                onChange={(e) => {
+                  setNumberOfAddition(e.target.value);
+                }}
+              >
+                <option value='3'>3</option>
+                <option value='6'>6</option>
+                <option value='9'>9</option>
+                <option value='12'>12</option>
+                <option value='15'>15</option>
+                <option value='15'>All</option>
+              </select>
+            </div>
+          </article>
 
-        <div className={styles['container-other-result']}>
-          {/* {dummyData.slice(0, numberOfData).map((data, i) => (
+          <div className={styles['container-other-result']}>
+            {/* {dummyData.slice(0, numberOfData).map((data, i) => (
             <article
               className={styles['container-other-result__wrapper']}
               onClick={() => {
@@ -261,49 +286,52 @@ function Result() {
             </article>
           ))} */}
 
-          <article className={styles.card}>
-            <div>
-              <h2 className={styles.card__heading}>83%</h2>
-            </div>
-            <div className={styles.card__wrapper}>
-              <h3 className={styles.card__subheading}>Leukimia</h3>
-              <AiFillQuestionCircle className={styles.card__icon} />
-              <ProgressBar progress='83' />
-            </div>
-          </article>
-          <article className={styles.card}>
-            <div>
-              <h2 className={styles.card__heading}>74%</h2>
-            </div>
-            <div className={styles.card__wrapper}>
-              <h3 className={styles.card__subheading}>Lung Cancer</h3>
-              <AiFillQuestionCircle className={styles.card__icon} />
-              <ProgressBar progress='74' />
-            </div>
-          </article>
-        </div>
-        <div
-          className={styles.container__footer}
-          onClick={() => {
-            if (numberOfData > dummyData.length) {
-              console.log('Full');
-            } else {
-              setNumberOfData(Number(numberOfData) + Number(NumberOfAddition));
-            }
-            console.log('numberOfData', numberOfData);
-          }}
-        >
-          <button className={styles.footer__button}>Show more results</button>
-          <button
-            className={styles.footer__button + ' ' + styles['button--grey']}
-            onClick={() => setModalClicked(true)}
+            <article className={styles.card}>
+              <div>
+                <h2 className={styles.card__heading}>83%</h2>
+              </div>
+              <div className={styles.card__wrapper}>
+                <h3 className={styles.card__subheading}>Leukimia</h3>
+                <AiFillQuestionCircle className={styles.card__icon} />
+                <ProgressBar progress='83' />
+              </div>
+            </article>
+            <article className={styles.card}>
+              <div>
+                <h2 className={styles.card__heading}>74%</h2>
+              </div>
+              <div className={styles.card__wrapper}>
+                <h3 className={styles.card__subheading}>Lung Cancer</h3>
+                <AiFillQuestionCircle className={styles.card__icon} />
+                <ProgressBar progress='74' />
+              </div>
+            </article>
+          </div>
+          <div
+            className={styles.container__footer}
+            onClick={() => {
+              if (numberOfData > dummyData.length) {
+                console.log('Full');
+              } else {
+                setNumberOfData(
+                  Number(numberOfData) + Number(NumberOfAddition)
+                );
+              }
+              console.log('numberOfData', numberOfData);
+            }}
           >
-            Download Results
-          </button>
-        </div>
-      </article>
+            <button className={styles.footer__button}>Show more results</button>
+            <button
+              className={styles.footer__button + ' ' + styles['button--grey']}
+              onClick={() => setModalClicked(true)}
+            >
+              Download Results
+            </button>
+          </div>
+        </article>
+      </section>
 
-      <article
+      <section
         className={styles.container + ' ' + styles['container--collect-data']}
       >
         <div>
@@ -346,7 +374,7 @@ function Result() {
           iconPosition='right'
           size='big'
         />
-      </article>
+      </section>
 
       {modalClicked && (
         <Modal
