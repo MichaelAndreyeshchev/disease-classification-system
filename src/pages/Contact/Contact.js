36e9690c12328baddc54f232a924c22c5a4616bc
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2022-12-04 22:50:51 +0700

    feat: modify form and add validation

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Contact.module.css';
-// import style from './Contact.module.css';
+import { Formik, Form, Field } from 'formik';
 import groupImage from '../../Images/group-image.png';
 import swal from 'sweetalert';
 
 const Contact = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
-
   const validateEmail = (email) => {
     const re =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -25,142 +20,41 @@ const Contact = () => {
     });
   };
 
-  const emptyInput = (field) => {
-    swal({
-      title: 'Oops...!',
-      text: `Please enter your ${field}!`,
-      icon: 'error',
-      button: 'OK!',
-    });
-  };
+  const handleFormValidation = (values) => {
+    const errors = {};
 
-  const invalidEmail = () => {
-    swal({
-      title: 'Oops...!',
-      text: 'Please enter a valid email address',
-      icon: 'error',
-      button: 'OK!',
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (name.length < 1) {
-      emptyInput('name');
-    } else if (email.length < 1) {
-      emptyInput('email');
-    } else if (!validateEmail(email)) {
-      invalidEmail();
-    } else if (subject.length < 1) {
-      emptyInput('subject');
-    } else if (message.length < 1) {
-      emptyInput('message');
-    } else {
-      success();
-      setName('');
-      setEmail('');
-      setSubject('');
-      setMessage('');
+    if (!values.name) {
+      errors.name = 'Please fill in your name.';
     }
+
+    if (!values.email || values.email === '') {
+      errors.email = 'Please fill in your email.';
+    } else if (!validateEmail(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!values.subject) {
+      errors.subject = 'Please fill the subject field.';
+    }
+
+    if (!values.message || values.message === '') {
+      errors.message = 'Please fill the message field.';
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    setSubmitting(true);
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      success();
+      setSubmitting(false);
+      resetForm();
+    }, 2000);
   };
 
   return (
-    // <div className={style.container}>
-    //   <div className={style.form}>
-    //     <div className={style.contactInfo}>
-    //       <h3 className={style.title}>Let's connect</h3>
-    //       <p className={style.text}>
-    //         We would love to know what you think about this website as it will
-    //         help us improve our services and website. Send us a message and we
-    //         will respond to it as soon as possible.
-    //       </p>
-
-    //       <div className={style.info}>
-    //         <div className={style.information}>
-    //           <h2>Email :</h2>
-    //           <p>Tech for good's email</p>
-    //         </div>
-    //         <div className={style.information}>
-    //           <h2>Fax :</h2>
-    //           <p>123-456-789</p>
-    //         </div>
-    //       </div>
-
-    //       <div className={style.socialMedia}>
-    //         <p>Follow us on</p>
-    //         <div className={style.socialIcons}>
-    //           <a href='#'>
-    //             <i className='fab fa-facebook-f'></i>
-    //           </a>
-    //           <a href='#'>
-    //             <i className='fab fa-twitter'></i>
-    //           </a>
-    //           <a href='#'>
-    //             <i className='fab fa-instagram'></i>
-    //           </a>
-    //           <a href='#'>
-    //             <i className='fab fa-linkedin-in'></i>
-    //           </a>
-    //         </div>
-    //       </div>
-    //     </div>
-
-    //     <div className={style.contactForm}>
-    //       <form action='index.html' autoComplete='off'>
-    //         <h3 className={style.title}>Contact us</h3>
-    //         <div className={style.inputContainer}>
-    //           <input
-    //             type='text'
-    //             className={style.input}
-    //             value={name}
-    //             onChange={(e) => setName(e.target.value)}
-    //             id='name'
-    //             placeholder='Enter your name.'
-    //             autoComplete='off'
-    //           />
-    //         </div>
-    //         <div className={style.inputContainer}>
-    //           <input
-    //             type='email'
-    //             className={style.input}
-    //             value={email}
-    //             onChange={(e) => setEmail(e.target.value)}
-    //             id='email'
-    //             placeholder='Enter your email address.'
-    //             autoComplete='off'
-    //           />
-    //         </div>
-    //         <div className={style.inputContainer}>
-    //           <input
-    //             type='text'
-    //             className={style.input}
-    //             value={subject}
-    //             onChange={(e) => setSubject(e.target.value)}
-    //             id='subject'
-    //             placeholder='Enter the subject.'
-    //             autoComplete='off'
-    //           />
-    //         </div>
-    //         <div className={(style.inputContainer, style.textarea)}>
-    //           <textarea
-    //             className={style.input}
-    //             value={message}
-    //             onChange={(e) => setMessage(e.target.value)}
-    //             id='message'
-    //             placeholder='Enter your message here.'
-    //             data-gramm_editor='false'
-    //           ></textarea>
-    //         </div>
-    //         <input
-    //           type='submit'
-    //           value='Send'
-    //           className={style.btn}
-    //           onClick={handleSubmit}
-    //         />
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
     <main className={styles.main}>
       <section className={`${styles.container} ${styles.section__message}`}>
         <h2 className={styles.container__heading}>Let's Connect!</h2>
@@ -177,94 +71,95 @@ const Contact = () => {
       </section>
 
       <section className={`${styles.container} ${styles.section__form}`}>
-        <form className='row g-3'>
-          <div className='col-md-6'>
-            <label
-              for='inputEmail4'
-              className={`form-label ${styles.form__label}`}
-            >
-              Full Name
-            </label>
-            <input
-              type='text'
-              className={`form-control ${styles.form__input}`}
-              id='inputEmail4'
-              placeholder='John Doe'
-            />
-          </div>
-          <div className='col-md-6'>
-            <label
-              for='inputPassword4'
-              className={`form-label ${styles.form__label}`}
-            >
-              Email
-            </label>
-            <input
-              type='email'
-              className={`form-control ${styles.form__input}`}
-              id='inputPassword4'
-              placeholder='john_doe@domain.com'
-            />
-          </div>
-          <div className='col-12'>
-            <label
-              for='inputAddress'
-              className={`form-label ${styles.form__label}`}
-            >
-              Subject of the matter
-            </label>
-            <input
-              type='text'
-              className={`form-control ${styles.form__input}`}
-              id='inputAddress'
-              placeholder='Collaboration Offer'
-            />
-          </div>
-          <div className='col-12'>
-            <label
-              for='inputAddress2'
-              className={`form-label ${styles.form__label}`}
-            >
-              Message
-            </label>
-            <textarea
-              className={`form-control ${styles.form__textarea}`}
-              id='exampleFormControlTextarea1'
-            ></textarea>
-          </div>
-          <div className='col-12'>
-            <button type='submit' className={`btn ${styles.form__button}`}>
-              Submit
-            </button>
-          </div>
-
-          {/* <article>
-            <div>
-              <label htmlFor='fullName'>Full Name</label>
-              <input type='text' name='fullName' placeholder='John Doe' />
-            </div>
-            <div>
-              <label htmlFor='email'>Full Name</label>
-              <input
-                type='email'
-                name='email'
-                placeholder='john_doe@domain.com'
-              />
-            </div>
-          </article>
-          <div>
-            <label htmlFor='subject'>Subject of the matter</label>
-            <input
-              type='text'
-              name='subject'
-              placeholder='Collaboration Offer'
-            />
-          </div>
-          <div>
-            <label htmlFor='message'>Subject of the matter</label>
-            <textarea name='message' cols='30' rows='10'></textarea>
-          </div> */}
-        </form>
+        <Formik
+          initialValues={{ name: '', email: '', subject: '', message: '' }}
+          validate={(values) => handleFormValidation(values)}
+          onSubmit={(values, actions) => handleSubmit(values, actions)}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <Form className='row g-3'>
+              <article className='row'>
+                <div className='col-md-6'>
+                  <label
+                    for='name'
+                    className={`form-label ${styles.form__label}`}
+                  >
+                    Full Name
+                  </label>
+                  <Field
+                    as='input'
+                    className={`form-control ${styles.form__input}`}
+                    name='name'
+                    placeholder='John Doe'
+                  />
+                  <span className={styles.errors}>
+                    {errors.name && touched.name && errors.name}
+                  </span>
+                </div>
+                <div className='col-md-6'>
+                  <label
+                    for='email'
+                    className={`form-label ${styles.form__label}`}
+                  >
+                    Email
+                  </label>
+                  <Field
+                    as='input'
+                    className={`form-control ${styles.form__input}`}
+                    name='email'
+                    placeholder='john_doe@domain.com'
+                  />
+                  <span className={styles.errors}>
+                    {errors.email && touched.email && errors.email}
+                  </span>
+                </div>
+              </article>
+              <div className='col-12'>
+                <label
+                  for='subject'
+                  className={`form-label ${styles.form__label}`}
+                >
+                  Subject of the matter
+                </label>
+                <Field
+                  as='input'
+                  className={`form-control ${styles.form__input}`}
+                  name='subject'
+                  placeholder='Collaboration Offer'
+                />
+                <span className={styles.errors}>
+                  {errors.subject && touched.subject && errors.subject}
+                </span>
+              </div>
+              <div className='col-12'>
+                <label
+                  for='message'
+                  className={`form-label ${styles.form__label}`}
+                >
+                  Message
+                </label>
+                <Field
+                  as='textarea'
+                  className={`form-control ${styles.form__textarea}`}
+                  name='message'
+                  placeholder='I would like to ask you...'
+                />
+                <span className={styles.errors}>
+                  {errors.message && touched.message && errors.message}
+                </span>
+              </div>
+              <div className='col-12'>
+                <button
+                  type='submit'
+                  className={`btn ${styles.form__button}`}
+                  disabled={isSubmitting}
+                >
+                  Submit
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </section>
     </main>
   );
