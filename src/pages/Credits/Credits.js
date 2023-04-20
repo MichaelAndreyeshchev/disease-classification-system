d2c63ebcaf5906c7762944660a3e80f3367886c1
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2023-04-20 20:32:19 +0700

    feat: add heading and text in credits page

--- src/pages/Credits/Credits.js
+++ src/pages/Credits/Credits.js
@@ -8,23 +8,52 @@ const Credits = () => {
   return (
     <main className={styles.main}>
       <section className={styles.section}>
+        <h1
+          className={`${styles.section__heading} ${styles['heading--center']}`}
+        >
+          Behind the wonder!
+        </h1>
+        <p
+          className={`${styles.section__description} ${styles['description--center']}`}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, magni?
+          Odio velit soluta error tempore!
+        </p>
+      </section>
+
+      <section className={styles.section}>
         <h1 className={styles.section__title}>Our Resources</h1>
         <TestiMonial />
       </section>
-      <section className={styles.section}>
-        <h2 className={styles.section__title}>Meet the Team</h2>
-        <article className={styles.wrapper}>
-          <Card
-            name='Michael Andrev'
-            role='Data Scientist'
-            description=''
-            variant='michael'
-          />
-          <Card
-            name='Joseph Haryanto'
-            role='Frontend developer'
-            description=''
-          />
+
+      <section className={`${styles.section} ${styles['section--team']}`}>
+        <article className={`${styles.container}`}>
+          <h2 className={styles.wrapper__heading}>
+            Guess who's behind the wonder?
+          </h2>
+          <p className={styles.wrapper__description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dictumst ac
+            pellentesque aliquet phasellus posuere commodo.
+          </p>
+          <article className={`${styles.wrapper}`}>
+            <Card
+              name='Michael Andrev'
+              role='Data Scientist'
+              description=''
+              variant='michael'
+            />
+            <Card
+              name='Joseph Haryanto'
+              role='Software Engineer'
+              description=''
+            />
+            <Card
+              name='Ernest Salim'
+              role='Software Engineer'
+              description=''
+              variant='michael'
+            />
+          </article>
         </article>
       </section>
     </main>
